aa459b5b22e2ec27e7c1ba733d4fdc615a4979ff
Mohamed Kamal <ku18mz@gmail.com>
2024-07-18 14:29:46 +0300

    First MVC Project

---
+++ labs/Server_Side/MVC/MVC_Project/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Project.Models;
+
+namespace MVC_Project.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ViewResult All()
+        {
+            ProductBL productBL = new ProductBL();
+            return View("AllProducts", productBL.GetAll());
+        }
+
+        public ViewResult Details(int id)
+        {
+            ProductBL productBL = new ProductBL();
+            return View("ProductDetails", productBL.GetById(id));
+        }
+    }
+}
---
+++ labs/Server_Side/MVC/MVC_Project/Models/ProductBL.cs
@@ -0,0 +1,25 @@
+﻿namespace MVC_Project.Models
+{
+    public class ProductBL
+    {
+        List<Product> products = new List<Product>();
+
+        public ProductBL()
+        {
+            products.Add(new Product { Id = 1, Name = "Replacement Voice Remote Control (2nd GEN) L5B83H", Price = 1050, ImageUrl = "product1.jpg" });
+            products.Add(new Product { Id = 2, Name = "Universal Remote Control Replacement for Samsung Smart-TV", Price = 870, ImageUrl = "product2.jpg" });
+            products.Add(new Product { Id = 3, Name = "Universal Remote Control Compatible for Samsung Smart-TV", Price = 676, ImageUrl = "product3.jpg" });
+        }
+
+
+        public List<Product> GetAll()
+        {
+            return products;
+        }
+
+        public Product GetById(int id)
+        {
+            return products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
