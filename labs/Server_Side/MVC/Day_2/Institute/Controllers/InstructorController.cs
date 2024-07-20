f12d456692ba304de3b0e9f3ad15a7116dea2f73
Mohamed Kamal <ku18mz@gmail.com>
2024-07-20 23:19:42 +0300

    add mvc Day_2 project

---
+++ labs/Server_Side/MVC/Day_2/Institute/Controllers/InstructorController.cs
@@ -0,0 +1,24 @@
+﻿using Institute.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Institute.Controllers
+{
+    public class InstructorController : Controller
+    {
+        private readonly InstituteContext context = new();
+
+        public IActionResult Index()
+        {
+            var allInstructors = context.Instructors.ToList();
+
+            return View("Index", allInstructors);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var instructor = context.Instructors.FirstOrDefault(ins => ins.Id == id);
+
+            return View("Details", instructor);
+        }
+    }
+}
---
+++ labs/Server_Side/MVC/Day_2/Institute/Models/InstituteContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Institute.Models
+{
+    public class InstituteContext: DbContext
+    {
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<Instructor> Instructors { get; set; }
+        public virtual DbSet<Trainee> Trainees { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<CourseResult> CourseResults { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-C2CGBOT\\SQLEXPRESS;Database=InstituteDB;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Instructor>().HasData(new Instructor() { Id = 1, Name = "Ali", ImageUrl = "Ins1.jpg", Salary = 5000, Address = "Cairo" });
+            modelBuilder.Entity<Instructor>().HasData(new Instructor() { Id = 2, Name = "Amr", ImageUrl = "Ins2.jpg", Salary = 15488, Address = "Alex" });
+            modelBuilder.Entity<Instructor>().HasData(new Instructor() { Id = 3, Name = "Ola", ImageUrl = "Ins3.jpg", Salary = 8000, Address = "Cairo" });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
